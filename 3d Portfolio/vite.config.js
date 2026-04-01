b9efa207db607bdcbd39757db74afc68e8248c3a
GurleenKaur655 <GKAUR09@mynbcc.ca>
2026-04-01 00:22:32 -0300

    fixed the chunk size issue

--- 3d Portfolio/vite.config.js
+++ 3d Portfolio/vite.config.js
@@ -4,8 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
