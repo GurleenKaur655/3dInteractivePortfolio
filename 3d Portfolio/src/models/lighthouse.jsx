ef7eb4532dbb2a2047846c194cbfbc67bd52d1d4
GurleenKaur655 <GKAUR09@mynbcc.ca>
2026-04-01 00:50:23 -0300

    Edited the motion

--- 3d Portfolio/src/models/lighthouse.jsx
+++ 3d Portfolio/src/models/lighthouse.jsx
@@ -11,7 +11,7 @@ isRotating, setIsRotating, setCurrentStage, ...props
     const { nodes, materials } = useGLTF(lighthouseScene)
     const lastX = useRef(0);
     const rotationSpeed = useRef(0);
-    const dampingFactor = 0.95;
+    const dampingFactor = 0.88;
 
      useFrame((_, delta) => {
       if(isRotating) {
@@ -36,21 +36,19 @@ isRotating, setIsRotating, setCurrentStage, ...props
 
 
     }
-    const handlePointerMove = (e) => {
-        e.stopPropagation();
-        e.preventDefault();
+   const handlePointerMove = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
 
-        if (isRotating) {
-            const clientX = e.touches ?
-                e.touches[0].clientX : e.clientX;
+  if (isRotating) {
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    const delta = (clientX - lastX.current) / viewport.width;
 
-            const delta = (clientX - lastX.current) /
-                viewport.width;
-            lighthouseRef.current.rotation.y += delta * 0.01 * Math.PI;
-            lastX.current = clientX;
-            rotationSpeed.current = delta * 0.01 * Math.PI;
-        }
-    }
+    lighthouseRef.current.rotation.y += delta * 0.01 * Math.PI;
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  }
+}
     const handleKeyDown = (e) => {
         if (e.key === 'ArrowLeft') {
             if (!isRotating) setIsRotating(true);
@@ -67,71 +65,58 @@ isRotating, setIsRotating, setCurrentStage, ...props
             setIsRotating(false);
         }
     }
-    useFrame(() => {
-        if (!isRotating) {
-            rotationSpeed.current *= dampingFactor;
-            if (Math.abs(rotationSpeed.current) < 0.001) {
-                rotationSpeed.current = 0;
-            }
-            lighthouseRef.current.rotation.y += rotationSpeed.current;
-        } else {
-            const rotation = lighthouseRef.current.rotation.y;
+   useFrame(() => {
+  if (!isRotating) {
+    rotationSpeed.current *= dampingFactor;
+    if (Math.abs(rotationSpeed.current) < 0.001) {
+      rotationSpeed.current = 0;
+    }
+    lighthouseRef.current.rotation.y += rotationSpeed.current;
+  } else {
+    const rotation = lighthouseRef.current.rotation.y;
+    const normalizedRotation =
+      ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
 
-            /**
-             * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
-             * The goal is to ensure that the rotation value remains within a specific range to
-             * prevent potential issues with very large or negative rotation values.
-             *  Here's a step-by-step explanation of what this code does:
-             *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
-             *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
-             *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
-             *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
-             *     This is done to ensure that the value remains positive and within the range of
-             *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
-             *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
-             *     modulo operation to the value obtained in step 2. This step guarantees that the value
-             *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
-             *     circle in radians.
-             */
-            const normalizedRotation =
-                ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
-
-            // Set the current stage based on the island's orientation
-            switch (true) {
-                case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
-                    setCurrentStage(4);
-                    break;
-                case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
-                    setCurrentStage(3);
-                    break;
-                case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
-                    setCurrentStage(2);
-                    break;
-                case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
-                    setCurrentStage(1);
-                    break;
-                default:
-                    setCurrentStage(null);
-            }
-        }
-
-    });
-
+    switch (true) {
+      case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+        setCurrentStage(4);
+        break;
+      case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+        setCurrentStage(3);
+        break;
+      case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+        setCurrentStage(2);
+        break;
+      case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+        setCurrentStage(1);
+        break;
+      default:
+        setCurrentStage(null);
+    }
+  }
+});
     useEffect(() => {
-        const canvas = gl.domElement;
-        canvas.addEventListener('pointerdown', handlePointerDown);
-        canvas.addEventListener('pointerup', handlePointerUp);
-        canvas.addEventListener('pointermove', handlePointerMove);
-        document.addEventListener('keydown', handleKeyDown)
-        document.addEventListener('keyup', handleKeyUp);
-        return () => {
-            canvas.removeEventListener(`pointerdown`, handlePointerDown);
-            canvas.removeEventListener(`pointerup`, handlePointerUp)
-            canvas.removeEventListener(`pointermove`, handlePointerMove)
-            document.removeEventListener('keyup', handleKeyUp)
-            document.removeEventListener('keydown', handleKeyDown);
-        }
-    }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
+  const canvas = gl.domElement;
+  canvas.addEventListener('pointerdown', handlePointerDown);
+  canvas.addEventListener('pointerup', handlePointerUp);
+  canvas.addEventListener('pointermove', handlePointerMove);
+  canvas.addEventListener('touchstart', handlePointerDown, { passive: false });
+  canvas.addEventListener('touchend', handlePointerUp, { passive: false });
+  canvas.addEventListener('touchmove', handlePointerMove, { passive: false });
+  document.addEventListener('keydown', handleKeyDown);
+  document.addEventListener('keyup', handleKeyUp);
+
+  return () => {
+    canvas.removeEventListener('pointerdown', handlePointerDown);
+    canvas.removeEventListener('pointerup', handlePointerUp);
+    canvas.removeEventListener('pointermove', handlePointerMove);
+    canvas.removeEventListener('touchstart', handlePointerDown);
+    canvas.removeEventListener('touchend', handlePointerUp);
+    canvas.removeEventListener('touchmove', handlePointerMove);
+    document.removeEventListener('keydown', handleKeyDown);
+    document.removeEventListener('keyup', handleKeyUp);
+  }
+}, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
     useEffect(() => {
         Object.values(materials).forEach((material) => {
             material.needsUpdate = true
